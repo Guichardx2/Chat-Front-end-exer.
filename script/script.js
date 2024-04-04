807d21cf63d798fb7157c097040cf426e7d1b1a8
Guichard <jeancharles.g.guichard@gmail.com>
2024-04-04 14:41:58 -0300

    mudanças e envio de mensagem

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let inp_message = document.querySelector("#message");
+let btnsend = document.querySelector("#btn-send");
+
+function sendMessage (event){
+    event.preventDefault();
+    
+    const newfield = document.createElement('div')
+    newfield.innerHTML = `<div class="message-chat"> <strong>Você diz:</strong> </div>
+    <div class="message-user"> ${inp_message.value} </div>`
+    
+    const list_message= document.querySelector("#first")
+    list_message.appendChild(newfield)
+    
+    if (inp_message.value.trim() !== ''){
+        inp_message.value = ''
+    }
+}
+btnsend.addEventListener("click", sendMessage)
