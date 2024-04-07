1bcb9e7cff3dd15701d0de2b68dda3c30cfd188e
Guichard <jeancharles.g.guichard@gmail.com>
2024-04-07 17:15:44 -0300

    mensagens e barra de rolagem

--- script/script.js
+++ script/script.js
@@ -2,13 +2,14 @@ let inp_message = document.querySelector("#message");
 let btnsend = document.querySelector("#btn-send");
 
 function sendMessage (event){
-    event.preventDefault();
+    event.preventDefault()
     
     const newfield = document.createElement('div')
+    newfield.classList.add("message-item-user")
     newfield.innerHTML = `<div class="message-chat"> <strong>Você diz:</strong> </div>
     <div class="message-user"> ${inp_message.value} </div>`
     
-    const list_message= document.querySelector("#first")
+    const list_message= document.querySelector(".chat-area") 
     list_message.appendChild(newfield)
     
     if (inp_message.value.trim() !== ''){
